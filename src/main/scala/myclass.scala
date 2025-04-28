c05b5306234477fd44137333f887695fe541f73c
Srivikram R <vikramsri347@gmail.com>
2025-04-28 21:55:29 +0530

    Pattern matching

--- src/main/scala/myclass.scala
+++ src/main/scala/myclass.scala
@@ -1,5 +1,38 @@
 object myclass {
   def main(args: Array[String]): Unit = {
-    print("hello World")
+    for(i<-1 to 5 by 1){
+      for(j<-1 to i){
+        print(i+" ")
+      }
+      println(" ")
+    }
+
+    var num = 1
+    for(i<-1 to 5){
+      for(j<-1 to i){
+        print(num+" ")
+        num = num + 1
+      }
+      println(" ")
+    }
+  }
+}
+
+
+object vikram {
+  def main(args: Array[String]): Unit = {
+    for(i<-1 to 5 by 1){
+      for(j<-1 to i){
+        print(j+" ")
+      }
+      println(" ")
+    }
+
+    for(i<-5 to 1 by -1){
+      for(j<-i to 1 by -1){
+        print(j+" ")
+      }
+      println(" ")
+    }
   }
 }
